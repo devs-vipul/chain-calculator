6a47ecb54b050667d3b2242d702afd1bce2142db
devs-vipul <devs.vipul@gmail.com>
2025-01-13 16:10:36 +0530

    connection dots

--- components/calculator.tsx
+++ components/calculator.tsx
@@ -81,7 +81,7 @@ export function Calculator() {
           </div>
 
           {/* Function Cards */}
-          <div className="col-span-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="col-span-3 grid grid-cols-1 md:m-auto md:grid-cols-2 lg:grid-cols-3 gap-8">
             {functions.map((func) => (
               <FunctionCard
                 key={func.id}
